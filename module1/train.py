739a6c8be691cd96cfbe3190dbdb3ed9d3221a9c
KAMAL SINGH <kamal.singh@avl.com>
2024-07-31 11:15:35 +0530

    initial commit

---
+++ module1/train.py
@@ -0,0 +1,27 @@
+# train.py
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+
+# Load data
+data = pd.read_csv('house_prices.csv')
+X = data[['feature1', 'feature2']]
+y = data['price']
+
+# Split data
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Train model
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+# Test model
+y_pred = model.predict(X_test)
+mse = mean_squared_error(y_test, y_pred)
+print(f'Mean Squared Error: {mse}')
+
+# Save model
+import joblib
+joblib.dump(model, 'model.joblib')
